3477b7fd83760062ff578bf3d9918aafcc2a6418
PrashanthJ11 <jangamprashanthjaya@gmail.com>
2023-11-22 13:26:39 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def prashu(request):
+    return render(request,'prashu.html')
+
+def lucky(request):
+    return render(request,'lucky.html')
+
+def ammu(request):
+    return render(request,'ammu.html')
+
+def rishi2(request):
+    return render(request,'rishi2.html')
+
